181cf99469c15486b8242d5351880e2568ef66b7
pikapikunova <nastya2000ru@mail.ru>
2021-05-14 01:41:11 +0300

    Show Location

--- app/src/main/java/com/example/gpsservice/MainActivity.kt
+++ app/src/main/java/com/example/gpsservice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.gpsservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var coordList: LinearLayout
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coordList = findViewById(R.id.coordList)
+        button = findViewById(R.id.button)
+        button.setOnClickListener{
+            GPSHelper.startLocationListening(this)
+            val tv = TextView(this)
+            tv.text = "Широта: ${GPSHelper.imHere?.latitude}; Долгота: ${GPSHelper.imHere?.longitude}"
+            coordList.addView(tv)
+        }
     }
 }
