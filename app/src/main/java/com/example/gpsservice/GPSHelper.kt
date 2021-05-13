61460aa4c353527a87645484e88bd9fb2de97fa5
pikapikunova <nastya2000ru@mail.ru>
2021-05-13 18:27:45 +0300

    First commit

---
+++ app/src/main/java/com/example/gpsservice/GPSHelper.kt
@@ -0,0 +1,42 @@
+package com.example.gpsservice
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+
+class GPSHelper: LocationListener{
+    companion object
+    {
+        var imHere: Location? = null
+
+        fun startLocationListening(context: Context)
+        {
+            val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500L, 10F, GPSHelper())
+
+            imHere = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+        imHere = location
+    }
+
+}
