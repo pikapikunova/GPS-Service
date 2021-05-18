05107d89c4632b487c4b1a2392755dc0aa2f1013
pikapikunova <nastya2000ru@mail.ru>
2021-05-18 23:38:34 +0300

    add shared preferences for radiobuttongroup

--- app/src/main/java/com/example/gpsservice/MainActivity.kt
+++ app/src/main/java/com/example/gpsservice/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     val listOfCoord = mutableMapOf("Кул-Шариф" to "55.798374 49.105159", "Двойка" to "55.792378 49.122228", "Деревня универсиады" to "55.7441869 49.1837646", "55.744453 49.187614" to "55.744453 49.187614")
     val APP_PREFERENCES_EDIT = "text"
+    val APP_PREFERENCES_Radio = "radioGroup"
     lateinit var prefs: SharedPreferences
 
     @RequiresApi(Build.VERSION_CODES.M)
@@ -123,18 +124,21 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         val text = findViewById<TextView>(R.id.textView2)
+        val radioGroup = findViewById<RadioGroup>(R.id.rbGroup)
         val editor = prefs.edit()
         editor.putString(APP_PREFERENCES_EDIT, text.text.toString()).apply()
-
+        editor.putInt(APP_PREFERENCES_Radio, radioGroup.checkedRadioButtonId).apply()
     }
 
     override fun onResume() {
         super.onResume()
         val text = findViewById<TextView>(R.id.textView2)
-        if (this.prefs.contains(APP_PREFERENCES_EDIT)) {
+        val radioGroup = findViewById<RadioGroup>(R.id.rbGroup)
+        if (this.prefs.contains(APP_PREFERENCES_EDIT))
             text.setText(this.prefs.getString(APP_PREFERENCES_EDIT, "0"))
 
-        }
+        if(this.prefs.contains(APP_PREFERENCES_Radio))
+            radioGroup.check(this.prefs.getInt(APP_PREFERENCES_Radio, 0))
     }
         private fun changeServiceState(forceStart: Boolean = false) {
         if (!LocationService.running || forceStart) {
